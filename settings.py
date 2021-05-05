1887ec9d9d2a9a7ca4c0c174ec402d48e6f36c88
Tolu-Orina <toluorina@gmail.com>
2021-05-05 13:07:56 +0100

    New3

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
-ALLOWED_HOSTS = ['irombi.herokuapp.com']
+ALLOWED_HOSTS = ['desolate-coast-43559.herokuapp.com']
 
 
 # Application definition
